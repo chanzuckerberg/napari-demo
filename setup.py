24fa55be724363f077a5e74f7f027f977bffca21
lauramarcos <63799148+lauramarcos@users.noreply.github.com>
2021-11-04 16:44:14 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    python_requires='>=3.6','>=3.7','>=3.8',
+    python_requires='>=3.6',
     install_requires=requirements,
     use_scm_version=use_scm,
     setup_requires=['setuptools_scm'],
